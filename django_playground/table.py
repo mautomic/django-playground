b45c50c330121938dfd90774faf15d9b02986886
mautomic <maurya373@gmail.com>
2020-12-20 20:11:59 -0600

    convert CSV to JSON in POST handler

--- django_playground/table.py
+++ django_playground/table.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseBadRequest
+import json
 
 
 def handler(request, id=None):
@@ -15,8 +16,34 @@ def get_table(id):
 
 
 def post_table(request):
+
+    header = request.GET.get('headerRow', 'False')
     raw_data = request.body
-    if raw_data is not None:
-        data = raw_data.decode("utf-8")
-        return HttpResponse("Submitting csv to task queue with data: " + data)
-    return HttpResponseBadRequest("Please send a post request with proper csv data")
+
+    if raw_data is None:
+        return HttpResponseBadRequest("Please send a post request with proper csv data")
+
+    json_data = {}
+
+    data = raw_data.decode("utf-8")
+    data_lines = data.splitlines()
+    data_start_index = 0
+
+    if header == 'True':
+        columns = split_by_comma(data_lines[0])
+        json_data['header'] = columns
+        data_start_index = 1
+
+    formatted_rows = []
+    for i in range(data_start_index, len(data_lines)):
+        row = data_lines[i]
+        formatted_row = split_by_comma(row)
+        formatted_rows.append(formatted_row)
+
+    json_data['rows'] = formatted_rows
+    return HttpResponse(json.dumps(json_data))
+
+
+def split_by_comma(str):
+    items = str.split(',')
+    return [x.strip() for x in items]
